f5efb33e0f021065ec9e121dfd89914d65c5de11
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-07-08 22:25:32 -0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,66 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+
+        def dfs(r, c):
+            size = 0
+            stack = collections.deque([(r, c)])
+            visited.add((r, c))
+
+            while stack:
+
+                r, c = stack.pop()
+                size += 1
+
+                for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+
+                    nr, nc = r + dr, c + dc
+
+                    if (
+                        0 <= nr < ROWS and 
+                        0 <= nc < COLS and 
+                        (nr, nc) not in visited and 
+                        grid[nr][nc] == 1
+                    ):
+
+                        visited.add((nr, nc))
+                        stack.append((nr, nc))
+            
+            return size
+
+        def bfs(r, c):
+            size = 0
+            queue = collections.deque([(r, c)])
+            visited.add((r, c))
+
+            while queue:
+                r, c = queue.popleft()
+                size += 1
+
+                for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nr, nc = r + dr, c + dc
+
+                    if (
+                        0 <= nr < ROWS and
+                        0 <= nc < COLS and
+                        (nr, nc) not in visited and
+                        grid[nr][nc] == 1
+                    ):
+                        visited.add((nr, nc))
+                        queue.append((nr, nc))
+
+            return size
+
+        max_size = 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    # max_size = max(max_size, bfs(r, c))
+                    max_size = max(max_size, dfs(r, c))
+
+        return max_size
