2a5d523958ff3d32ebe4ef9bc3132ae5d2b510a1
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-07-13 05:34:36 -0700

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+        
+        def climb(step):
+
+            if step == n:
+                return 1
+            
+            if step > n:
+                return 0
+            
+            if step in memo:
+                return memo[step]
+
+            memo[step] = climb(step + 1) + climb(step + 2)
+
+            return memo[step]
+        
+        # return climb(0)
+
+        tab = {n: 1}
+
+        for step in reversed(range(n)):
+
+            tab[step] = tab.get(step + 1, 0) + tab.get(step + 2, 0)
+
+        return tab[0]
+
