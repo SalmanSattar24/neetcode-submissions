911ef8005ac5a396910027d9fd5ce231a019df23
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-07-14 02:57:32 -0700

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        N = len(s)
+        memo = {}
+
+        def recurse(index):
+
+            if index >= N:
+                return 1
+
+            if s[index] == '0':
+                return 0
+            
+            if index in memo:
+                return memo[index]
+            
+            take_one = recurse(index + 1)
+
+            take_two = 0
+            if index + 1 < N and int(s[index : index + 2]) <= 26:
+                take_two = recurse(index + 2)
+
+            memo[index] = take_one + take_two
+
+            return memo[index]
+        
+        return recurse(0)
