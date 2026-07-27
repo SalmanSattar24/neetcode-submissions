75cf5da4aa9bba54ac58fcdf2930a181438e3749
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-07-26 19:02:09 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        
+        indegree = [0] * numCourses
+        adj = {i : [] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+
+            indegree[prereq] += 1
+            adj[course].append(prereq)
+        
+
+        queue = deque()
+        
+        for i in range(numCourses):
+
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        completed = 0
+        order = []
+
+        while queue:
+
+            course = queue.popleft()
+            completed += 1
+            order.append(course)
+
+            for dependant in adj[course]:
+
+                indegree[dependant] -= 1
+
+                if indegree[dependant] == 0:
+
+                    queue.append(dependant)
+
+        print(order)
+        
+        if completed == numCourses:
+            return list(reversed(order))
+        else:
+            return []
