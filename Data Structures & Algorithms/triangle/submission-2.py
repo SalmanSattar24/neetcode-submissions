41554d90ff17a6f9a6ad9e4ab719a1645a2d8cda
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-08-04 19:40:39 -0700

    Add: triangle - submission-2

---
+++ Data Structures & Algorithms/triangle/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+        R = len(triangle)
+        memo = defaultdict(int)
+        for c in range(len(triangle[R - 1])):
+            memo[(R, c)] = 0
+
+
+            
+        for r in reversed(range(R)):
+            for c in reversed(range(len(triangle[r]))):
+
+                key = (r, c)
+                memo[key] = triangle[r][c] + (
+                    min(memo.get((r + 1, c), 0), memo.get((r + 1, c + 1), 0))
+                )
+
+        
+        return memo[(0, 0)]
