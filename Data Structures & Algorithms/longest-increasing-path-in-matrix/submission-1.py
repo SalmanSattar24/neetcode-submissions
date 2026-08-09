b38f2fcb40d0b97b116ddf0a41683453f4ee6e10
Salman Abdus Sattar <46921674+SalmanSattar24@users.noreply.github.com>
2026-08-08 18:39:39 -0700

    Add: longest-increasing-path-in-matrix - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        
+        sys.setrecursionlimit(20000)
+        
+        R, C = len(matrix), len(matrix[0])
+        memo = {}
+
+        def recurse(r, c, prev_val):
+
+            if (
+                min(r, c) < 0 or 
+                r >= R or c >= C or
+                matrix[r][c] <= prev_val
+            ):
+                return 0
+            
+            key = (r, c)
+            if key in memo:
+                return memo[key]
+            
+            right = 1 + recurse(r, c + 1, matrix[r][c])
+            left = 1 + recurse(r, c - 1, matrix[r][c])
+            up = 1 + recurse(r - 1, c, matrix[r][c])
+            down = 1 + recurse(r + 1, c, matrix[r][c])
+
+            memo[key] = max(right, left, up, down)
+            return memo[key]
+
+        
+        for r in range(R):
+            for c in range(C):
+
+                recurse(r, c, -1)
+
+        return max(memo.values())
